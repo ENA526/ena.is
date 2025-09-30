144569f7c935b73ecf182477d3403e29abfd020e
Moses Kim <sangonkim1998@gmail.com>
2025-10-01 00:12:44 +1000

    added links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,7 @@
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M3.75 17.25h16.5" /></svg>
 				</summary>
 				<div class="absolute right-0 mt-2 w-48 rounded-xl border border-slate-200 bg-white shadow-lg p-2">
-					<a class="block rounded-lg px-3 py-2 hover:bg-slate-50" href="#features">Link 1</a>
+					<a class="block rounded-lg px-3 py-2 hover:bg-slate-50" href="/minesweeper">Minesweeper</a>
 					<a class="block rounded-lg px-3 py-2 hover:bg-slate-50" href="#components">Link 2</a>
 					<a class="block rounded-lg px-3 py-2 hover:bg-slate-50" href="#cta">Link 3</a>
 				</div>
@@ -47,9 +47,9 @@
 		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between gap-4">
 			<p>© 2025 ENA. All rights reserved.</p>
 			<ul class="flex gap-4">
-				<li><a class="hover:underline" href="#">GitHub</a></li>
-				<li><a class="hover:underline" href="#">Discord</a></li>
-				<li><a class="hover:underline" href="#">X</a></li>
+				<li><a class="hover:underline" href="https://github.com/ENA526">GitHub</a></li>
+				<li><a class="hover:underline" href="https://discord.gg/jEUu7Btg">Discord</a></li>
+				<li><a class="hover:underline" href="https://twitter.com/ENA526">X</a></li>
 			</ul>
 		</div>
 	</footer>
